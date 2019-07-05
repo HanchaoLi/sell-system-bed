0e46f65c7607cae9ae900b100418fe848229868f
hanchao.li <hanchao.li@borngroup.com>
2019-07-05 15:51:48 -0400

    implement get list of category API

--- product/src/main/java/com/example/product/controller/ProductController.java
+++ product/src/main/java/com/example/product/controller/ProductController.java
@@ -4,6 +4,9 @@ public class ProductController {
 
     /**
      * 1. get list of product data from MySQL
+     * 2. get category type list
+     * 3. get category
+     * 4. construct data
      */
     public void listOfProduct() {
 
