94ccd74587b751f53fe1868f544ccb4d0cfb8d5d
hanchao.li <hanchao.li@borngroup.com>
2019-07-05 15:39:43 -0400

    add product interface, get list of product from mysql

---
+++ product/src/main/java/com/example/product/controller/ProductController.java
@@ -0,0 +1,11 @@
+package com.example.product.controller;
+
+public class ProductController {
+
+    /**
+     * 1. get list of product data from MySQL
+     */
+    public void listOfProduct() {
+
+    }
+}
---
+++ product/src/test/java/com/example/product/repository/ProductInfoRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.example.product.repository;
+
+import com.example.product.entity.ProductInfo;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductInfoRepositoryTest {
+
+    @Autowired
+    private ProductInfoRepository productInfoRepository;
+
+    @Test
+    public void findByProductStatus() throws Exception{
+        List<ProductInfo> prod = productInfoRepository.findByProductStatus(0);
+        Assert.assertTrue(prod.size() > 0);
+    }
+}
