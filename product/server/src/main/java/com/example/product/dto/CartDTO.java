beeb12241683d0c6cf5c02881b465147ad8439a0
HanchaoLi <seraphme1234@gmail.com>
2019-07-08 17:00:36 -0400

    refactor product service to different module

--- product/server/src/main/java/com/example/product/dto/CartDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.product.dto;
-
-import lombok.Data;
-
-@Data
-public class CartDTO {
-    private String productId;
-    private Integer productQuantity;
-}
--- product/server/src/main/java/com/example/product/service/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.example.product.service;
-
-import com.example.product.entity.ProductCategory;
-
-import java.util.List;
-
-public interface CategoryService {
-    List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList);
-}
