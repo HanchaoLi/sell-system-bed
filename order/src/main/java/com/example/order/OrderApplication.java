2672c0a6d71a79544e3559321fc55bc1a26aaab5
hanchao.li <hanchao.li@borngroup.com>
2019-08-29 15:23:35 -0400

    set hystri breaker

--- order/src/main/java/com/example/order/OrderApplication.java
+++ order/src/main/java/com/example/order/OrderApplication.java
@@ -2,12 +2,14 @@ package com.example.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class OrderApplication {
 
     public static void main(String[] args) {
---
+++ order/src/main/java/com/example/order/controller/HystrixController.java
@@ -0,0 +1,27 @@
+package com.example.order.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@RestController
+public class HystrixController {
+
+    @HystrixCommand(fallbackMethod = "fallback")
+    @GetMapping("/getProductInfoList")
+    public String getProductInfo() {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.postForObject("http://localhost:9000/product/listForOrder",
+                Arrays.asList("157875196366160022"),
+                String.class);
+
+    }
+
+    private String fallback() {
+        return "too crowded, please try later";
+    }
+
+}
